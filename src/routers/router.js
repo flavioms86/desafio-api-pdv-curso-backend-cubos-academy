c399995213fb579d6252ad259519d4c608806b7c
Everton Silva <slvaeverton@gmail.com>
2023-11-03 19:39:47 -0300

    implementando rota de cadastro de usuario

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,7 +1,9 @@
-const testAPI = require("../controllers/testAPI");
+const { testAPI, registerUser } = require("../controllers/testAPI");
 
 const router = require("express").Router();
 
 router.get("/api", testAPI);
+
+router.post("/usuario", registerUser)
 
 module.exports = router;
