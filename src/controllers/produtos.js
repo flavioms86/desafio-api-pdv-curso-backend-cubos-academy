ccbc9222577002b9d5503a053be8625ffdb6d202
Flavio Silva <flavioms86@hotmail.com>
2023-11-13 09:18:33 -0300

    refactor: alterar retorno chamada controlador produtos

--- src/controllers/produtos.js
+++ src/controllers/produtos.js
@@ -1,50 +1,67 @@
 const provider = require("../database/providers");
 
-
 const registerProducts = async (req, res) => {
-    const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
 
-    try {
-        const verifyCategoria = await provider.verifyProductsProvider(categoria_id);
-        const verifyProduct = await provider.verifyProductsUpdateProvider(id);
+  try {
+    const verifyCategoria = await provider.verifyProductsProvider(categoria_id);
+    const verifyProduct = await provider.verifyProductsUpdateProvider(id);
 
-        if (!verifyCategoria) {
-            return res.status(404).json({ mensagem: "O id da categoria informada não existe." });
-        };
-        if (!verifyProduct) {
-            return res.status(404).json({ mensagem: "O id do produto informado não existe." });
-        }
-        const product = await provider.createProductProvider(descricao, quantidade_estoque, valor, categoria_id);
-        return res.status(201).json(product);
-    } catch (error) {
-        return res.status(500).json({ mensagem: "Erro interno no servidor." });
+    if (!verifyCategoria) {
+      return res
+        .status(404)
+        .json({ mensagem: "O id da categoria informada não existe." });
     }
+    if (!verifyProduct) {
+      return res
+        .status(404)
+        .json({ mensagem: "O id do produto informado não existe." });
+    }
+    const product = await provider.createProductProvider(
+      descricao,
+      quantidade_estoque,
+      valor,
+      categoria_id
+    );
+    return res.status(201).json(product);
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno no servidor." });
+  }
 };
 
 const updateProducts = async (req, res) => {
-    const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
-    const id = req.params.id;
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
+  const id = req.params.id;
 
-    try {
-        const verifyCategoria = await provider.verifyProductsProvider(categoria_id);
-        const verifyProduct = await provider.verifyProductsUpdateProvider(id);
+  try {
+    const verifyCategory = await provider.verifyProductsProvider(categoria_id);
+    const verifyProduct = await provider.verifyProductsUpdateProvider(id);
 
-        if (!verifyCategoria) {
-            return res.status(404).json({ mensagem: "O id da categoria informada não existe." });
-        }
-        if (!verifyProduct) {
-            return res.status(404).json({ mensagem: "O id do produto informado não existe." });
-        }
-
-        const product = await provider.updateProductProvider(id, descricao, quantidade_estoque, valor, categoria_id);
-        return res.status(200).json(product);
-    } catch (error) {
-        return res.status(500).json({ mensagem: "Erro interno no servidor." });
+    if (!verifyCategory) {
+      return res
+        .status(404)
+        .json({ mensagem: "O id da categoria informada não existe." });
     }
+    if (!verifyProduct) {
+      return res
+        .status(404)
+        .json({ mensagem: "O id do produto informado não existe." });
+    }
+
+    const product = await provider.updateProductProvider(
+      id,
+      descricao,
+      quantidade_estoque,
+      valor,
+      categoria_id
+    );
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno no servidor." });
+  }
 };
 
-
 module.exports = {
-    registerProducts,
-    updateProducts
+  registerProducts,
+  updateProducts,
 };
--- src/database/providers/produtos.js
+++ src/database/providers/produtos.js
@@ -1,48 +1,56 @@
 const Knex = require("../knex/index");
 
 const verifyProductsProvider = async (id) => {
-    const result = await Knex("categorias").where({ id }).first();
+  const result = await Knex("categorias").where({ id }).first();
 
-    if (result) {
-        return result;
-    } else {
-        return 0;
-    }
+  if (result) {
+    return result;
+  } else {
+    return 0;
+  }
 };
 
 const verifyProductsUpdateProvider = async (id) => {
-    const result = await Knex("produtos").where({ id }).first();
+  const result = await Knex("produtos").where({ id }).first();
 
-    if (result) {
-        return result;
-    } else {
-        return 0;
-    }
+  if (result) {
+    return result;
+  } else {
+    return 0;
+  }
 };
 
-const createProductProvider = async (descricao, quantidade_estoque, valor, categoria_id) => {
-    const result = await Knex("produtos")
-        .insert({ descricao, quantidade_estoque, valor, categoria_id })
-        .returning("*");
+const createProductProvider = async (
+  descricao,
+  quantidade_estoque,
+  valor,
+  categoria_id
+) => {
+  const result = await Knex("produtos")
+    .insert({ descricao, quantidade_estoque, valor, categoria_id })
+    .returning("*");
 
-    const productsRegistered = result[0];
-    return productsRegistered;
+  return result[0];
 };
 
-const updateProductProvider = async (id, descricao, quantidade_estoque, valor, categoria_id) => {
-    const result = await Knex("produtos")
-        .update({ descricao, quantidade_estoque, valor, categoria_id })
-        .where({ id: id })
-        .returning("*");
+const updateProductProvider = async (
+  id,
+  descricao,
+  quantidade_estoque,
+  valor,
+  categoria_id
+) => {
+  const result = await Knex("produtos")
+    .update({ descricao, quantidade_estoque, valor, categoria_id })
+    .where({ id: id })
+    .returning("*");
 
-    const productsRegistered = result[0];
-    return productsRegistered;
+  return result[0];
 };
 
 module.exports = {
-    verifyProductsProvider,
-    createProductProvider,
-    updateProductProvider,
-    verifyProductsUpdateProvider
-}
-
+  verifyProductsProvider,
+  createProductProvider,
+  updateProductProvider,
+  verifyProductsUpdateProvider,
+};
