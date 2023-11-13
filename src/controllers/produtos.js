849050161dbb80b6332a2da633aa6de7c8270571
Flavio Silva <flavioms86@hotmail.com>
2023-11-13 08:57:29 -0300

    fix: alterar validação joi rota cadastro produto

--- src/controllers/produtos.js
+++ src/controllers/produtos.js
@@ -1,23 +1,28 @@
 const provider = require("../database/providers");
 
-
 const registerProducts = async (req, res) => {
-    const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
 
-    try {
-        const verifyCategoria = await provider.verifyProductsProvider(categoria_id);
+  try {
+    const verifyCategoria = await provider.verifyProductsProvider(categoria_id);
 
-        if (!verifyCategoria) {
-            return res.status(404).json({ mensagem: "O id da categoria informada não existe." });
-        }
-        const product = await provider.createProductProvider(descricao, quantidade_estoque, valor, categoria_id);
-        return res.status(201).json(product);
-    } catch (error) {
-        console.log(error.message);
-        return res.status(500).json({ mensagem: "Erro interno no servidor." });
+    if (!verifyCategoria) {
+      return res
+        .status(404)
+        .json({ mensagem: "O id da categoria informada não existe." });
     }
+    const product = await provider.createProductProvider(
+      descricao,
+      quantidade_estoque,
+      valor,
+      categoria_id
+    );
+    return res.status(201).json(product);
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno no servidor." });
+  }
 };
 
 module.exports = {
-    registerProducts
+  registerProducts,
 };
--- src/utils/joi_schemas/cadastroProdutos.js
+++ src/utils/joi_schemas/cadastroProdutos.js
@@ -1,22 +1,32 @@
 const joi = require("joi");
 
 const productsRegister = joi.object({
-    descricao: joi.string().empty().required().messages({
-        "any.required": "O campo descrição é obrigatório",
-        "string.empty": "O campo descrição não pode ser vazio",
+  descricao: joi.string().empty().required().messages({
+    "any.required": "O campo descrição é obrigatório",
+    "string.empty": "O campo descrição não pode ser vazio",
+  }),
+  quantidade_estoque: joi
+    .number()
+    .positive()
+    .integer()
+    .strict()
+    .required()
+    .messages({
+      "any.required": "O campo quantidade_estoque é obrigatório",
+      "number.base": "O campo quantidade_estoque precisa ser um número",
+      "number.positive": "A quantidade de estoque deve ser um número positivo",
+      "number.integer": "A quantidade em estoque deve ser um número inteiro.",
     }),
-    quantidade_estoque: joi.number().required().messages({
-        "any.required": "O campo quantidade_estoque é obrigatório",
-        "number.base": "O campo quantidade_estoque precisa ser um número",
-    }),
-    valor: joi.number().required().messages({
-        "any.required": "O campo valor é obrigatório",
-        "number.base": "O campo valor precisa ser um número",
-    }),
-    categoria_id: joi.number().required().messages({
-        "any.required": "O campo categoria_id é obrigatório",
-        "number.base": "O campo categoria_id precisa ser um número",
-    }),
+  valor: joi.number().positive().integer().strict().required().messages({
+    "any.required": "O campo valor é obrigatório",
+    "number.base": "O campo valor precisa ser um número",
+    "number.positive": "A valor deve ser um número positivo",
+    "number.integer": "A valor deve ser um número inteiro.",
+  }),
+  categoria_id: joi.number().required().strict().messages({
+    "any.required": "O campo categoria_id é obrigatório",
+    "number.base": "O campo categoria_id precisa ser um número",
+  }),
 });
 
 module.exports = productsRegister;
