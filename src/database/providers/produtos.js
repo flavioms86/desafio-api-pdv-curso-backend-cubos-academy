a78645c5c6401c65ea01634f82a9dfac7f41b11c
Flavio Silva <flavioms86@hotmail.com>
2023-11-13 09:41:48 -0300

    refactor: alterar passagem propriedade id na chamada do knex

--- src/database/providers/produtos.js
+++ src/database/providers/produtos.js
@@ -1,61 +1,69 @@
 const Knex = require("../knex/index");
 
 const verifyProductsProvider = async (id) => {
-    const result = await Knex("categorias").where({ id }).first();
+  const result = await Knex("categorias").where({ id }).first();
 
-    if (result) {
-        return result;
-    } else {
-        return 0;
-    }
+  if (result) {
+    return result;
+  } else {
+    return 0;
+  }
 };
 
 const verifyProductsIdProvider = async (id) => {
-    const result = await Knex("produtos").where({ id }).first();
+  const result = await Knex("produtos").where({ id }).first();
 
-    if (result) {
-        return result;
-    } else {
-        return 0;
-    }
+  if (result) {
+    return result;
+  } else {
+    return 0;
+  }
 };
 
-const createProductProvider = async (descricao, quantidade_estoque, valor, categoria_id) => {
-    const result = await Knex("produtos")
-        .insert({ descricao, quantidade_estoque, valor, categoria_id })
-        .returning("*");
+const createProductProvider = async (
+  descricao,
+  quantidade_estoque,
+  valor,
+  categoria_id
+) => {
+  const result = await Knex("produtos")
+    .insert({ descricao, quantidade_estoque, valor, categoria_id })
+    .returning("*");
 
-    const productsRegistered = result[0];
-    return productsRegistered;
+  const productsRegistered = result[0];
+  return productsRegistered;
 };
 
-const updateProductProvider = async (id, descricao, quantidade_estoque, valor, categoria_id) => {
-    const result = await Knex("produtos")
-        .update({ descricao, quantidade_estoque, valor, categoria_id })
-        .where({ id: id })
-        .returning("*");
+const updateProductProvider = async (
+  id,
+  descricao,
+  quantidade_estoque,
+  valor,
+  categoria_id
+) => {
+  const result = await Knex("produtos")
+    .update({ descricao, quantidade_estoque, valor, categoria_id })
+    .where({ id: id })
+    .returning("*");
 
-    const productsRegistered = result[0];
-    return productsRegistered;
+  const productsRegistered = result[0];
+  return productsRegistered;
 };
 
 const deleteProductProvider = async (id) => {
-    const result = await Knex("produtos")
-        .where({ id: id })
-        .del();
+  const result = await Knex("produtos").where({ id }).del();
 
-    if (result) {
-        return result;
-    } else {
-        return 0;
-    }
+  if (result) {
+    return result;
+  } else {
+    return 0;
+  }
 };
 
 module.exports = {
-    verifyProductsProvider,
-    createProductProvider,
-    updateProductProvider,
-    verifyProductsIdProvider,
-    deleteProductProvider
-}
-
+  verifyProductsProvider,
+  createProductProvider,
+  updateProductProvider,
+  verifyProductsIdProvider,
+  deleteProductProvider,
+};
