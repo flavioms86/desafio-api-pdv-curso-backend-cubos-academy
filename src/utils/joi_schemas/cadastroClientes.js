36c9d73f8dc3be8788cc091ad6ee20902663b7b2
Flavio Silva <flavioms86@hotmail.com>
2023-11-16 20:31:04 -0300

    fix: alterar validação string vazia cadastro e atualizacao de produtos

--- src/utils/joi_schemas/cadastroClientes.js
+++ src/utils/joi_schemas/cadastroClientes.js
@@ -11,8 +11,7 @@ const clientRegister = joi.object({
       "string.base": "O campo nome deve ser do tipo string",
       "string.empty": "O campo nome é obrigatório",
       "string.min": "O campo nome deve ter um tamanho mínimo de 3 caracteres.",
-      "string.pattern.base":
-        "O campo nome deve conter apenas letras com ou sem acentuação.",
+      "string.pattern.base": "O campo nome deve conter apenas letras com ou sem acentuação.",
     }),
   email: joi.string().email().required().messages({
     "string.email": "O campo email precisa ser em um formato valido",
@@ -34,26 +33,32 @@ const clientRegister = joi.object({
       "string.base": "O campo cep deve ser do tipo string",
       "string.length": "O campo cep deve ter 9 caracteres no formato '00000-000'.",
       "string.pattern.base": "O campo cep deve ser no formato '00000-000'.",
+      "string.empty": "O cep nome não pode ser vazio.",
     }),
   rua: joi.string().max(60).optional().messages({
     "string.base": "O campo rua deve ser do tipo string",
     "string.max": "O campo rua deve ter no máximo 60 caracteres.",
+    "string.empty": "O campo rua não pode ser vazio.",
   }),
   numero: joi.string().max(10).optional().messages({
     "string.base": "O campo numero deve ser do tipo string",
     "string.max": "O campo numero deve ter no máximo 10 caracteres.",
+    "string.empty": "O campo número não pode ser vazio.",
   }),
   bairro: joi.string().max(60).optional().messages({
     "string.base": "O campo bairro deve ser do tipo string",
     "string.max": "O campo bairro deve ter no máximo 60 caracteres.",
+    "string.empty": "O campo bairro não pode ser vazio.",
   }),
   cidade: joi.string().max(50).optional().messages({
     "string.base": "O campo cidade deve ser do tipo string",
     "string.max": "O campo cidade deve ter no máximo 50 caracteres.",
+    "string.empty": "O campo cidade não pode ser vazio.",
   }),
   estado: joi.string().max(15).optional().messages({
     "string.base": "O campo estado deve ser do tipo string",
     "string.max": "O campo estado deve ter no máximo 15 caracteres.",
+    "string.empty": "O campo estado não pode ser vazio.",
   }),
 });
 
