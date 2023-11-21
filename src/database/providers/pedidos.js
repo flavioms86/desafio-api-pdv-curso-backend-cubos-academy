177bd7b0459c99fe2c7ff62548ca94983d196fe8
DevFelipreis <lfreis.contato@gmail.com>
2023-11-20 22:16:39 -0300

    feat:reduce em getAllOrder

--- src/database/providers/pedidos.js
+++ src/database/providers/pedidos.js
@@ -2,8 +2,8 @@ const { id } = require("../../utils/joi_schemas/loginUsuario");
 const Knex = require("../knex/index");
 
 const getAllOrder = async () => {
-    const results = await Knex("pedido_produtos")
-        .leftJoin("pedidos", "pedido_produtos.id", "=", "pedidos.id")
+    const results = await Knex("pedidos")
+        .leftJoin("pedido_produtos", "pedidos.id", "=", "pedido_produtos.pedido_id")
         .groupBy("pedidos.id", "pedido_produtos.id")
         .select(
             "pedidos.id as pedido_id",
@@ -17,24 +17,42 @@ const getAllOrder = async () => {
             "pedido_produtos.produto_id"
         );
 
-    const groupedResults = results.map((row) => ({
-        pedido: {
-            id: row.pedido_id,
-            valor_total: row.valor_total,
-            observacao: row.observacao,
-            cliente_id: row.cliente_id,
-        },
-        pedido_produtos: {
-            id: row.pedido_produto_id,
-            quantidade_produto: row.quantidade_produto,
-            valor_produto: row.valor_produto,
-            pedido_id: row.pedido_produto_pedido_id,
-            produto_id: row.produto_id,
-        },
-    }));
+    const groupedResults = results.reduce((acum, row) => {
+        const existingOrder = acum.find(order => order.pedido.id === row.pedido_id);
+
+        if (existingOrder) {
+            existingOrder.pedido_produtos.push({
+                id: row.pedido_produto_id,
+                quantidade_produto: row.quantidade_produto,
+                valor_produto: row.valor_produto,
+                pedido_id: row.pedido_produto_pedido_id,
+                produto_id: row.produto_id,
+            });
+        } else {
+            acum.push({
+                pedido: {
+                    id: row.pedido_id,
+                    valor_total: row.valor_total,
+                    observacao: row.observacao,
+                    cliente_id: row.cliente_id,
+                },
+                pedido_produtos: [{
+                    id: row.pedido_produto_id,
+                    quantidade_produto: row.quantidade_produto,
+                    valor_produto: row.valor_produto,
+                    pedido_id: row.pedido_produto_pedido_id,
+                    produto_id: row.produto_id,
+                }],
+            });
+        }
+
+        return acum;
+    }, []);
 
     return groupedResults;
 };
+
+
 
 const getOrderById = async (id_cliente) => {
     const resultOrder = await Knex("pedidos")
