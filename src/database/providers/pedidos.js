ff10eca8fe8724a7ae7d89ccbfaeb008d8242238
DevFelipreis <lfreis.contato@gmail.com>
2023-11-22 19:40:39 -0300

    fix: Corrigindo duas buscas no banco de dados

--- src/database/providers/pedidos.js
+++ src/database/providers/pedidos.js
@@ -55,34 +55,55 @@ const getAllOrder = async () => {
 
 
 const getOrderById = async (id_cliente) => {
-    const resultOrder = await Knex("pedidos")
-        .select("id", "valor_total", "observacao", "cliente_id")
+    const results = await Knex("pedidos")
+        .leftJoin("pedido_produtos", "pedidos.id", "=", "pedido_produtos.pedido_id")
+        .groupBy("pedidos.id", "pedido_produtos.id")
+        .select(
+            "pedidos.id as pedido_id",
+            "pedidos.valor_total",
+            "pedidos.observacao",
+            "pedidos.cliente_id",
+            "pedido_produtos.id as pedido_produto_id",
+            "pedido_produtos.quantidade_produto",
+            "pedido_produtos.valor_produto",
+            "pedido_produtos.pedido_id as pedido_produto_pedido_id",
+            "pedido_produtos.produto_id"
+        )
         .where("cliente_id", "=", id_cliente);
 
-    const resultOrderProducts = await Knex("pedido_produtos").select(
-        "id",
-        "quantidade_produto",
-        "valor_produto",
-        "pedido_id",
-        "produto_id"
-    );
+    const groupedResults = results.reduce((acum, row) => {
+        const existingOrder = acum.find(order => order.pedido.id === row.pedido_id);
 
-    const formatarDados = resultOrder.map((order) => {
-        const result = resultOrderProducts.filter(
-            (orderProduct) => orderProduct.pedido_id == order.id
-        );
-        return {
-            pedido: {
-                id: order.id,
-                valor_total: order.valor_total,
-                observacao: order.observacao,
-                cliente_id: order.cliente_id,
-            },
-            pedido_produtos: result,
-        };
-    });
+        if (existingOrder) {
+            existingOrder.pedido_produtos.push({
+                id: row.pedido_produto_id,
+                quantidade_produto: row.quantidade_produto,
+                valor_produto: row.valor_produto,
+                pedido_id: row.pedido_produto_pedido_id,
+                produto_id: row.produto_id,
+            });
+        } else {
+            acum.push({
+                pedido: {
+                    id: row.pedido_id,
+                    valor_total: row.valor_total,
+                    observacao: row.observacao,
+                    cliente_id: row.cliente_id,
+                },
+                pedido_produtos: [{
+                    id: row.pedido_produto_id,
+                    quantidade_produto: row.quantidade_produto,
+                    valor_produto: row.valor_produto,
+                    pedido_id: row.pedido_produto_pedido_id,
+                    produto_id: row.produto_id,
+                }],
+            });
+        }
 
-    return formatarDados;
+        return acum;
+    }, []);
+
+    return groupedResults;
 };
 
 module.exports = {
