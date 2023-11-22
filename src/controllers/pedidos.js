33171d49325140af1e0f522c01002a3592375de6
Flavio Silva <flavioms86@hotmail.com>
2023-11-22 19:27:56 -0300

    chore: testando outras configuracoes

--- src/controllers/pedidos.js
+++ src/controllers/pedidos.js
@@ -2,73 +2,118 @@ const provider = require("../database/providers");
 const { htmlCompiler } = require("../utils/email_notifications/compilation_html");
 const transporter = require("../utils/email_notifications/email_connection");
 
-const registerRequest = async (req, res) => {
-  
-  try {
-    const {
-      cliente_id,
-      observacao,
-      pedido_produtos
-    } = req.body;
-    
+// const registerRequest = async (req, res) => {
+//     try {
+//         const { cliente_id, observacao, pedido_produtos } = req.body;
 
-    if (!cliente_id || !pedido_produtos || pedido_produtos.length === 0) {
-      return res
-        .status(400)
-        .json({ mensagem: "Campos obrigatórios não informados." });
-    }
+//         if (!cliente_id || !pedido_produtos || pedido_produtos.length === 0) {
+//             return res
+//                 .status(400)
+//                 .json({ mensagem: "Campos obrigatórios não informados." });
+//         }
+
+//         const clientExists = await provider.verifyClientsProvider(cliente_id);
+
+//         if (!clientExists) {
+//             return res.status(404).json({ mensagem: "Cliente não encontrado." });
+//         }
+
+//         //for of para verificar estoque
+//         for (const produtoPedido of pedido_produtos) {
+//             const { produto_id, quantidade_produto } = produtoPedido;
+//             const productExists = await provider.verifyProductForRequest(produto_id);
+
+//             if (!productExists) {
+//                 return res.status(404).json({
+//                     mensagem: "Produto não encontrado.",
+//                 });
+//             }
+
+//             if (
+//                 productExists &&
+//                 productExists.quantidade_estoque < quantidade_produto
+//             ) {
+//                 return res.status(404).json({
+//                     mensagem: `Quantidade insuficiente em estoque para ${productExists.descricao}`,
+//                 });
+//             }
+//             await provider.createRequestProvider(
+//                 cliente_id,
+//                 observacao,
+//                 pedido_produtos
+//             );
+//         }
+
+//         const html = await htmlCompiler(
+//             "src/utils/email_notifications/pedido.html",
+//             {
+//                 destinatario: clientExists.nome,
+//             }
+//         );
+
+//         transporter.sendMail({
+//             from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
+//             to: `${clientExists.nome} <${clientExists.email}>`,
+//             subject: "Pedido efetuado com sucesso!",
+//             html,
+//         });
+
+//         return res.status(201).json({ mensagem: "Pedido cadastrado com sucesso!" });
+//     } catch (error) {
+//         console.log(error.message);
+//         return res.status(500).json({ mensagem: "Erro interno no servidor." });
+//     }
+// };
+
+const registerOrder = async (req, res) => {
+    const { cliente_id, observacao, pedido_produtos } = req.body;
 
     const clientExists = await provider.verifyClientsProvider(cliente_id);
-    
 
     if (!clientExists) {
-      return res.status(404).json({ mensagem: "Cliente não encontrado." });
-    }
- 
-    //for of para verificar estoque
-    for (const produtoPedido of pedido_produtos) {
-      const { produto_id, quantidade_produto } = produtoPedido;
-      const productExists = await provider.verifyProductForRequest(produto_id);
-      
-      if (!productExists) {
-        return res.status(404).json({
-          mensagem: "Produto não encontrado."
-        });
-      }
-
-      if (
-        productExists &&
-        productExists.quantidade_estoque < quantidade_produto
-      ) {
-        return res.status(404).json({
-          mensagem: `Quantidade insuficiente em estoque para ${productExists.descricao}`,
-        });
-      }
-      await provider.createRequestProvider(
-        cliente_id,
-        observacao,
-        pedido_produtos
-      );
+        return res
+            .status(404)
+            .json({ mensagem: "Não existe cliente para o id informado." });
     }
 
-      
-    const html = await htmlCompiler("src/utils/email_notifications/pedido.html", {
-      destinatario: clientExists.nome,
-    });
+    console.log(pedido_produtos);
 
-    transporter.sendMail({
-      from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
-      to: `${clientExists.nome} <${clientExists.email}>`,
-      subject: "Pedido efetuado com sucesso!",
-      html,
-    });
+    if (pedido_produtos.length === 0) {
+        return res.status(400).json({
+            mensagem: "Deve ser informado a lista de produtos a ser feito o pedido",
+        });
+    }
+    const produtosPedido = [];
+    for (const pedProduto of pedido_produtos) {
+        const result = await provider.verifyProductsIdProvider(
+            pedProduto.produto_id
+        );
 
-    
-    return res.status(201).json({ mensagem: "Pedido cadastrado com sucesso!" });
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).json({ mensagem: "Erro interno no servidor." });
-  }
+        if (!result) {
+            return res.status(400).json({
+                mensagem: `Não existe o produto com o id ${pedProduto.produto_id}.`,
+            });
+        }
+
+        if (result.quantidade_estoque < pedProduto.quantidade_produto) {
+            return res.status(400).json({
+                mensagem: `Quantidade insuficiente no estoque para o produto "${result.descricao}"`,
+            });
+        }
+
+        produtosPedido.push({
+            id: result.id,
+            quantidade_estoque: result.quantidade_estoque,
+            valor: result.valor,
+            quantidade: pedProduto.quantidade_produto,
+        });
+    }
+
+    const registrarPedido = await provider.createOrder(cliente_id, observacao);
+
+    await provider.createOrderProducts(registrarPedido[0].id, produtosPedido);
+
+    return res.status(201).json("tudo certo.. eu acho");
+    // return res.status(201).json(produtosID);
 };
-
-module.exports = registerRequest;
+module.exports = registerOrder;
--- src/database/providers/pedidos.js
+++ src/database/providers/pedidos.js
@@ -1,15 +1,15 @@
 const Knex = require("../knex/index");
 
 const verifyProductForRequest = async (produto_id) => {
-  const result = await Knex("produtos").where({ id: produto_id }).first();
-  if (result) {
-    return result;
-  } else {
-    return 0;
-  }
+    const result = await Knex("produtos").where({ id: produto_id }).first();
+    if (result) {
+        return result;
+    } else {
+        return 0;
+    }
 };
 
-
+// const createOrder = async() ;
 
 // const createRequestProvider = async (
 //   cliente_id,
@@ -18,7 +18,6 @@ const verifyProductForRequest = async (produto_id) => {
 //   quantidade_produto,
 //   valor_produto
 // ) => {
-  
 
 //   const [result] = await Knex("pedidos").insert({
 //     cliente_id,
@@ -34,46 +33,75 @@ const verifyProductForRequest = async (produto_id) => {
 //   });
 // };
 
-const createRequestProvider = async (
-  cliente_id,
-  observacao,
-  pedido_produtos
-) => {
+// const createRequestProvider = async (cliente_id, observacao, pedido_produtos) => {
+//     const [pedido_id] = await Knex("pedidos").insert({
+//         cliente_id,
+//         observacao,
+//         valor_total: 0,
+//     });
 
-  const [pedido_id] = await Knex("pedidos").insert({
-    cliente_id,
-    observacao,
-    valor_total: 0 
-  });
+//     const totalValue = pedido_produtos.reduce((total, produto) => {
+//         return total + produto.quantidade_produto * produto.valor_produto;
+//     }, 0);
 
+//     console.log(totalValue);
 
-  const totalValue = pedido_produtos.reduce((total, produto) => {
-    return total + produto.quantidade_produto * produto.valor_produto;
-  }, 0);
+//     await Knex("pedidos")
+//         .where({ id: pedido_id })
+//         .update({ valor_total: totalValue });
 
-  await Knex("pedidos").where({ id: pedido_id }).update({ valor_total: totalValue });
+//     for (const produtoPedido of pedido_produtos) {
+//         const { produto_id, quantidade_produto, valor_produto } = produtoPedido;
 
+//         await Knex("pedido_produtos").insert({
+//             pedido_id,
+//             produto_id,
+//             quantidade_produto,
+//             valor_produto,
+//         });
+//     }
+// };
 
-  for (const produtoPedido of pedido_produtos) {
-    const { produto_id, quantidade_produto, valor_produto } = produtoPedido;
-
-    await Knex("pedido_produtos").insert({
-      pedido_id,
-      produto_id,
-      quantidade_produto,
-      valor_produto,
-    });
-  }
+const createOrder = async (cliente_id, observacao) => {
+    const pedido = await Knex("pedidos")
+        .insert({ cliente_id, observacao, valor_total: 0 })
+        .returning("*");
+    return pedido;
 };
 
+const createOrderProducts = async (pedido_id, produtos) => {
+    let valorTotal = 0;
+    for (const produto of produtos) {
+        await Knex("pedido_produtos").insert({
+            pedido_id,
+            produto_id: produto.id,
+            quantidade_produto: produto.quantidade,
+            valor_produto: produto.valor,
+        });
+        valorTotal += produto.valor;
+        await updateProductStockAfterOrder(produto.id, produto.quantidade);
+    }
 
+    await updateTotalOrderPrice(pedido_id, valorTotal);
+    return;
+};
 
+const updateProductStockAfterOrder = async (produto_id, quantidade_estoque) => {
+    await Knex("produtos")
+        .decrement({ quantidade_estoque })
+        .where({ id: produto_id });
+    return;
+};
 
-
+const updateTotalOrderPrice = async (pedido_id, valorTotal) => {
+    await Knex("pedidos")
+        .update({ valor_total: valorTotal })
+        .where({ id: pedido_id });
+    return;
+};
 
 module.exports = {
-  verifyProductForRequest,
-  createRequestProvider,
- 
-  // createRequestProductProvider,
+    verifyProductForRequest,
+    createOrderProducts,
+    createOrder,
 };
